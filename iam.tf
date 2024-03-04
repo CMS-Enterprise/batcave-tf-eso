a94ab82cc8beddb62ad241f74375ea9baf5944c6
Shanawaze <shanawaze.shaik@revacomm.com>
2024-03-04 10:36:55 -0600

    adding providers

--- iam.tf
+++ iam.tf
@@ -1,5 +1,5 @@
 module "external_secrets_irsa" {
-  source                        = "git::git@github.com:CMS-Enterprise/batcave-tf-irsa.git//.?ref=1.0.1"
+  source                        = "git::git@github.com:CMS-Enterprise/batcave-tf-irsa.git//.?ref=main"
   role_name                     = "${var.cluster_name}-external-secrets"
   role_path                     = var.iam_path
   role_permissions_boundary_arn = var.permissions_boundary
--- variables.tf
+++ variables.tf
@@ -8,14 +8,22 @@ variable "cluster_name" {
   description = "Name of EKS cluster"
 }
 
+variable "cluster_endpoint" {
+  description = "Endpoint for EKS cluster"
+}
+
+variable "cluster_certificate_authority_data" {
+  description = "CA certificate data for EKS cluster"
+}
+
 variable "helm_namespace" {
   description = "Namespace for Helm chart"
-  default     = "kube-system"
+  default     = "external-secrets"
 }
 
 variable "helm_name" {
   description = "Name for Helm release"
-  default     = "external_secrets"
+  default     = "external-secrets"
 }
 
 variable "tolerations" {
@@ -41,7 +49,7 @@ variable "permissions_boundary" {
 variable "serviceAccountName" {
   description = "Service account for the ESO to connect to AWS IAM"
   type        = string
-  default     = "imagepull-es"
+  default     = "ub-imagepull-es"
 }
 
 variable "external_secrets_service_accounts" {
