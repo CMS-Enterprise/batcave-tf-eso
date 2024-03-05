39635aba2989ce84f7cb095e3311a77e811413fe
shanawazeshaik <106613833+sshaikRC@users.noreply.github.com>
2024-03-05 11:38:24 -0600

    Batiai 2296 - files for ESO (#1)
    * updating chart url
    
    * updating name for eso role
    
    * adding providers
    
    * updating tag ref for batcave-tf-irsa
    
    ---------
    
    Co-authored-by: Shanawaze <shanawaze.shaik@revacomm.com>

--- eso.tf
+++ eso.tf
@@ -1,35 +1,139 @@
 
 resource "helm_release" "external_secrets_operator" {
-  namespace  = var.helm_namespace
-  name       = var.helm_name
-  repository = "https://github.com/external-secrets/external-secrets"
-  chart      = "external-secrets"
+  namespace        = var.helm_namespace
+  name             = var.helm_name
+  repository       = "https://charts.external-secrets.io"
+  version          = "v0.9.13"
+  chart            = "external-secrets"
+  create_namespace = true
   set {
     name  = "image.repository"
     value = var.imagerepo
   }
   set {
-    name  = "controller.serviceAccount.name"
+    name  = "installCRDs"
+    value = "true"
+  }
+  set {
+    name  = "crds.createClusterExternalSecret"
+    value = "true"
+  }
+  set {
+    name  = "crds.createClusterSecretStore"
+    value = "true"
+  }
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+  set {
+    name  = "serviceAccount.name"
     value = var.serviceAccountName
   }
   set {
-    name  = "node.serviceAccount.name"
-    value = var.serviceAccountName
-  }
-  set {
-    name  = "controller.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
     value = module.external_secrets_irsa.iam_role_arn
   }
-  set {
-    name  = "node.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = module.external_secrets_irsa.iam_role_arn
-  }
+  # set {
+  #   name  = "webhook.serviceAccount.name"
+  #   value = var.serviceAccountName
+  # }
+  # set {
+  #   name  = "webhook.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+  #   value = module.external_secrets_irsa.iam_role_arn
+  # }
+  # set {
+  #   name  = "certController.serviceAccount.name"
+  #   value = var.serviceAccountName
+  # }
+  # set {
+  #   name  = "certController.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+  #   value = module.external_secrets_irsa.iam_role_arn
+  # }
+  
 
   dynamic "set" {
     for_each = var.tolerations
     content {
-      name  = "controller.tolerations[${set.key}].key"
+      name  = "tolerations[${set.key}].key"
       value = set.value.key
+    }
+  }
+  dynamic "set" {
+    for_each = var.tolerations
+    content {
+      name  = "webhook.tolerations[${set.key}].key"
+      value = set.value.key
+    }
+  }
+  dynamic "set" {
+    for_each = var.tolerations
+    content {
+      name  = "certController.tolerations[${set.key}].key"
+      value = set.value.key
+    }
+  }
+  dynamic "set" {
+    for_each = var.tolerations
+    content {
+      name  = "tolerations[${set.key}].value"
+      value = try(set.value.value, "")
+    }
+  }
+  dynamic "set" {
+    for_each = var.tolerations
+    content {
+      name  = "webhook.tolerations[${set.key}].value"
+      value = try(set.value.value, "")
+    }
+  }
+  dynamic "set" {
+    for_each = var.tolerations
+    content {
+      name  = "certController.tolerations[${set.key}].value"
+      value = try(set.value.value, "")
+    }
+  }
+  dynamic "set" {
+    for_each = var.tolerations
+    content {
+      name  = "tolerations[${set.key}].operator"
+      value = set.value.operator
+    }
+  }
+  dynamic "set" {
+    for_each = var.tolerations
+    content {
+      name  = "webhook.tolerations[${set.key}].operator"
+      value = set.value.operator
+    }
+  }
+  dynamic "set" {
+    for_each = var.tolerations
+    content {
+      name  = "certController.tolerations[${set.key}].operator"
+      value = set.value.operator
+    }
+  }
+  dynamic "set" {
+    for_each = var.tolerations
+    content {
+      name  = "tolerations[${set.key}].effect"
+      value = try(set.value.effect, "NoSchedule")
+    }
+  }
+  dynamic "set" {
+    for_each = var.tolerations
+    content {
+      name  = "webhook.tolerations[${set.key}].effect"
+      value = try(set.value.effect, "NoSchedule")
+    }
+  }
+  dynamic "set" {
+    for_each = var.tolerations
+    content {
+      name  = "certController.tolerations[${set.key}].effect"
+      value = try(set.value.effect, "NoSchedule")
     }
   }
 }
--- iam.tf
+++ iam.tf
@@ -1,10 +1,9 @@
 module "external_secrets_irsa" {
-  source                        = "git::git@github.com:CMS-Enterprise/batcave-tf-irsa.git//.?ref=BATIAI-2296"
-  role_name                     = "${var.cluster_name}-ub-external-secrets"
+  source                        = "git::git@github.com:CMS-Enterprise/batcave-tf-irsa.git//.?ref=1.1.0"
+  role_name                     = "${var.cluster_name}-external-secrets"
   role_path                     = var.iam_path
   role_permissions_boundary_arn = var.permissions_boundary
   app_name                      = "ESO"
-  #ssm_secret_arns               = 
   attach_secretsmanager_policy  = true
   oidc_providers = {
     main = {
